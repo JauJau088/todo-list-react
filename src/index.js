d9a3bb0027bb9fa9d7ae4fafa7e0b8d7672f5a08
jaujau088 <jaujau088@gmail.com>
2022-05-30 11:29:08 +0200

    Add state, refactor index.js to latest ReactDOM format

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import * as ReactDOM from 'react-dom/client';
 import TodoContainer from "./components/TodoContainer"
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>
+);
